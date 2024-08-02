9ba6beabf2bb2f7bf9dcec882fd8f9c0aa8df28b
ShiYiShe <yiiiiatb@gmail.com>
2024-08-02 20:35:28 +0800

    redux管理token

---
+++ react-jike/src/store/modules/user.js
@@ -0,0 +1,33 @@
+// 用户相关状态管理
+
+import { createSlice } from '@reduxjs/toolkit'
+import { request } from '@/utils'
+
+const userStore = createSlice({
+  name: "user",
+  initialState: {
+    token: ''
+  },
+  reducers: {
+    setToken (state, action) {
+      state.token = action.payload
+    }
+  }
+})
+
+const { setToken } = userStore.actions
+
+const userReducer = userStore.reducer
+
+const fetchLogin = (loginForm) => {
+  return async (dispatch) => {
+    // 发送请求
+    const res = await request.post('/authorizations', loginForm)
+    // 提交action存入token
+    dispatch(setToken(res.data.token))
+  }
+}
+
+export { fetchLogin, setToken }
+
+export default userReducer
