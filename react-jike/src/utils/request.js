a4c40b9d9a341725a697fb16e0477ee5fe4ffc6a
ShiYiShe <yiiiiatb@gmail.com>
2024-08-03 12:38:05 +0800

    axios请求头注入token

--- react-jike/src/utils/request.js
+++ react-jike/src/utils/request.js
@@ -3,6 +3,7 @@
 // 3. 请求拦截器 / 响应拦截器
 
 import axios from 'axios'
+import { getToken } from './token'
 
 const request = axios.create({
   baseURL: 'http://geek.itheima.net/v1_0',
@@ -11,6 +12,10 @@ const request = axios.create({
 
 // 添加请求拦截器
 request.interceptors.request.use((config)=> {
+  const token = getToken();
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}}`;
+  }
   return config
 }, (error)=> {
   return Promise.reject(error)
