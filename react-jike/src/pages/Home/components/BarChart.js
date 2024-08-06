207e2862c111c25ae2b9ac2d969417443057757c
ShiYiShe <yiiiiatb@gmail.com>
2024-08-06 15:30:49 +0800

    Echarts组件封装

---
+++ react-jike/src/pages/Home/components/BarChart.js
@@ -0,0 +1,35 @@
+// 柱状图组件
+
+import * as echarts from 'echarts';
+import { useEffect, useRef } from 'react';
+
+const BarChart = ({ title, yData }) => {
+  const chartRef = useRef(null);
+  // 渲染完毕后执行，保证DOM节点可用
+  useEffect(() => {
+    const chartDom = chartRef.current
+    const myChart = echarts.init(chartDom);
+    const option = {
+      title: {
+        text: title
+      },
+      xAxis: {
+        type: 'category',
+        data: ['Vue', 'React', 'Angular']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: yData,
+          type: 'bar'
+        }
+      ]
+    };
+    option && myChart.setOption(option);
+  }, [])
+  return <div ref={chartRef} style={{ width: '500px', height: '400px' }}></div>
+}
+
+export default BarChart;
--- react-jike/src/pages/Home/index.js
+++ react-jike/src/pages/Home/index.js
@@ -1,31 +1,13 @@
-import * as echarts from 'echarts';
-import { useEffect, useRef } from 'react';
+import BarChart from "./components/BarChart";
 
 const Home = () => {
-  const chartRef = useRef(null);
-  // 渲染完毕后执行，保证DOM节点可用
-  useEffect(() => {
-    const chartDom = chartRef.current
-    const myChart = echarts.init(chartDom);
-    const option = {
-      xAxis: {
-        type: 'category',
-        data: ['Vue', 'React', 'Angular']
-      },
-      yAxis: {
-        type: 'value'
-      },
-      series: [
-        {
-          data: [1, 2, 3],
-          type: 'bar'
-        }
-      ]
-    };
-    option && myChart.setOption(option);
-  }, [])
   // 保证节点有宽高
-  return <div><div ref={chartRef} style={{ width: '500px', height: '400px' }}></div></div>
+  return (
+    <div>
+      <BarChart title={'三大框架满意度'} yData={[1, 2, 3]} />
+      <BarChart title={'三大框架使用度'} yData={[3, 2, 1]} />
+    </div>
+  )
 }
 
 export default Home;
