601c2090273f1b5a3e50e1ec7a4e931175b4f770
ShiYiShe <yiiiiatb@gmail.com>
2024-08-07 19:16:32 +0800

    文章列表-状态条件渲染

--- react-jike/src/pages/Article/index.js
+++ react-jike/src/pages/Article/index.js
@@ -13,6 +13,10 @@ const { Option } = Select
 const { RangePicker } = DatePicker
 
 const Article = () => {
+  const status = {
+    1: <Tag color='warning'>待审核</Tag>,
+    2: <Tag color='success'>审核通过</Tag>
+  }
   const { channelList } = useChannel();
   // 准备列数据
   const columns = [
@@ -32,7 +36,8 @@ const Article = () => {
     {
       title: '状态',
       dataIndex: 'status',
-      render: data => <Tag color="green">审核通过</Tag>
+      // render: data => data === 1 ? <Tag color='warning'>待审核</Tag> : <Tag color='success'>审核通过</Tag>
+      render: data => status[data]
     },
     {
       title: '发布时间',
