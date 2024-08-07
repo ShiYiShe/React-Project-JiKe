088da5d87fe8bf2eb71bed5f66a86a928c493678
ShiYiShe <yiiiiatb@gmail.com>
2024-08-07 18:28:09 +0800

    文章列表-自定义hook获取频道列表

---
+++ react-jike/src/hooks/useChannel.js
@@ -0,0 +1,20 @@
+// 获取频道列表
+import { useEffect, useState } from "react";
+import { getChannelAPI } from "@/apis/article";
+
+function useChannel() {
+  const [channelList, setChannelList] = useState([]);
+
+  useEffect(() => {
+    const getChannelList = async () => {
+      const res = await getChannelAPI();
+      setChannelList(res.data.channels);
+    }
+    getChannelList();
+  }, [])
+  return {
+    channelList
+  }
+}
+
+export { useChannel };
--- react-jike/src/pages/Article/index.js
+++ react-jike/src/pages/Article/index.js
@@ -5,11 +5,13 @@ import locale from 'antd/es/date-picker/locale/zh_CN'
 import { Table, Tag, Space } from 'antd'
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons'
 import img404 from '@/assets/error.png'
+import { useChannel } from '@/hooks/useChannel'
 
 const { Option } = Select
 const { RangePicker } = DatePicker
 
 const Article = () => {
+  const { channelList } = useChannel();
   // 准备列数据
   const columns = [
     {
@@ -101,11 +103,9 @@ const Article = () => {
           <Form.Item label="频道" name="channel_id">
             <Select
               placeholder="请选择文章频道"
-              defaultValue="lucy"
-              style={{ width: 120 }}
+              style={{ width: 266 }}
             >
-              <Option value="jack">Jack</Option>
-              <Option value="lucy">Lucy</Option>
+              {channelList.map(item => <Option key={item.id} value={item.id}>{item.name}</Option>)}
             </Select>
           </Form.Item>
 
